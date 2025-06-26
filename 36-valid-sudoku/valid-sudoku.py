b7958ead80ade7e6e60a508c3ccba6ae5e6f954a
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2025-06-26 11:25:09 -0400

    Time: 3 ms (82.40%) | Memory: 18.1 MB (5.30%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -6,22 +6,30 @@ class Solution:
         for i in range(row):
             a = {}
             b = {}
-            c = {}
+            d = {}
+
             for j in range(col):
                 if board[i][j] != ".":
                     if board[i][j] not in a:
                         a[board[i][j]] = 1
                     else:
                         return False
+                    
                 if board[j][i] != ".":
                     if board[j][i] not in b:
                         b[board[j][i]] = 1
                     else:
                         return False
                 
-                if board[(i//3) * 3 + j//3][(i%3) * 3 + j % 3] != ".":
-                    if board[(i//3) * 3 + j//3][(i%3) * 3 + j % 3]  not in c:
-                        c[board[(i//3) * 3 + j//3][(i%3) * 3 + j % 3] ] = 1
+                c = 3 * (i % 3) + j % 3
+                r = (j // 3) + 3 * (i // 3)
+
+                # print(r, c, d)
+
+                if board[r][c] != ".":
+                    if board[r][c] not in d:
+                        d[board[r][c]] = 1
                     else:
                         return False
+            
         return True
