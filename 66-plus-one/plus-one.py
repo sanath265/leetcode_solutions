27fa78ce0012b4fc0fe579439aeb38e1047308c9
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2026-01-02 09:55:54 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (81.98%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,6 +4,22 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        a = int("".join(list(map(str, digits))))
-        return list(map(int, list(str(a+1))))
+        if digits[-1] + 1 < 10:
+            digits[-1] += 1
+            return digits
+        i = len(digits) - 1
+        res = 10
+        while(res > 9 and i >= 0):
+            res = digits[i] + 1
+            if res == 10:
+                digits[i] = 0
+            else:
+                digits[i] = res
+            i -= 1
+  
+        if i == -1 and res == 10:
+            digits = [1] + digits
+        
+        return digits
+
         
