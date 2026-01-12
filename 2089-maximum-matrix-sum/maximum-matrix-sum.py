6e13c2f466996ed4d6cd935cbdc378d69911ea09
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2026-01-12 15:12:01 -0700

    Time: 128 ms (11.97%) | Memory: 18.2 MB (98.93%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -4,40 +4,69 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: int
         """
-        colNeg = 0
-        col = len(matrix[0])
-        row = len(matrix)
-        c = 0
-        mini = 100001
-        for i in range(len(matrix)):
-            negCount = 0
-            for j in range(len(matrix[0])):
-                c += abs(matrix[i][j])
-                mini = min(mini, abs(matrix[i][j]))
-                if matrix[i][j] < 0:
-                    negCount += 1
-                
-            if col %2 == 1:
-                if negCount %2 == 1:
-                    colNeg += 1
-            else:
-                if negCount %2 == 0:
-                    colNeg += 1
-        
-        # print(col, colNeg, c)
 
-        if col %2 == 1:
-            if colNeg %2 == 1:
-                colNeg = 1
-            else:
-                colNeg = 0
+        row = len(matrix)
+        col = len(matrix[0])
+
+        total = 0
+        mini = float("inf")
+        colNeg = 0
+        for i in range(row):
+            neg = 0
+            for j in range(col):
+                total += abs(matrix[i][j])
+                mini = min(mini, abs(matrix[i][j])) 
+                if matrix[i][j] < 0:
+                    neg += 1
+            if neg%2 ==1:
+                colNeg += 1
+        
+        if colNeg % 2 == 1:
+            return total - 2 * mini
         else:
-            if colNeg %2 == 1:
-                colNeg = 1
-            else:
-                colNeg = 0
-        if colNeg == 0:
-            return c
-        else:
-            return c - 2*mini
+            return total
+
+
+
+
+
+
+
+
+        # colNeg = 0
+        # col = len(matrix[0])
+        # row = len(matrix)
+        # c = 0
+        # mini = 100001
+        # for i in range(len(matrix)):
+        #     negCount = 0
+        #     for j in range(len(matrix[0])):
+        #         c += abs(matrix[i][j])
+        #         mini = min(mini, abs(matrix[i][j]))
+        #         if matrix[i][j] < 0:
+        #             negCount += 1
+                
+        #     if col %2 == 1:
+        #         if negCount %2 == 1:
+        #             colNeg += 1
+        #     else:
+        #         if negCount %2 == 0:
+        #             colNeg += 1
+        
+        # # print(col, colNeg, c)
+
+        # if col %2 == 1:
+        #     if colNeg %2 == 1:
+        #         colNeg = 1
+        #     else:
+        #         colNeg = 0
+        # else:
+        #     if colNeg %2 == 1:
+        #         colNeg = 1
+        #     else:
+        #         colNeg = 0
+        # if colNeg == 0:
+        #     return c
+        # else:
+        #     return c - 2*mini
         
