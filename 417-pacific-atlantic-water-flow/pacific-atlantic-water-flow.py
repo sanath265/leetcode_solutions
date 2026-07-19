ce76a34d7a3bd4fe0ad1ba86ba08b67328b69d1e
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2026-07-19 10:28:33 -0400

    Time: 22 ms (90.82%) | Memory: 22.3 MB (38.84%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,44 +1,42 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-
+        
         row = len(heights)
         col = len(heights[0])
 
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        ans = []
-        dp = [[-1 for _ in range(col)] for _ in range(row)]
-
-        def dfs(i, j, visited):
-
-            if (i, j) in visited:
-                return
-            
-            visited.add((i, j))
-            for x, y in directions:
-                ni = i+x
-                nj = j + y
-
-                if ni >= 0 and nj >= 0 and ni < row and nj < col and heights[ni][nj] >= heights[i][j]:
-                    dfs(ni, nj, visited)
+        directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
 
         pacific = set()
         atlantic = set()
+
+        def dfs(i, j, setocean):
+            
+            if (i, j) in setocean:
+                return
+            setocean.add((i, j))
+            
+            for m, n in directions:
+                if 0 <= i+m < row and 0 <= j+n < col and heights[i+m][j+n] >= heights[i][j]:
+                    dfs(i+m, j+n, setocean)
+        
         for i in range(row):
             dfs(i, 0, pacific)
             dfs(i, col-1, atlantic)
+            
+        for j in range(col):
+            dfs(0, j, pacific)
+            dfs(row-1, j, atlantic)
         
-        for i in range(col):
-            dfs(0, i, pacific)
-            dfs(row - 1, i, atlantic)
-
         # print(pacific, atlantic)
 
-        return list(pacific.intersection(atlantic))
+        ans = []
+        for i , j in pacific:
+            if (i, j) in atlantic:
+                ans.append([i, j])
+        return ans
 
 
-    
 
-
-
+            
+            
         
