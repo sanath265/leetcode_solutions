839c6d3efec6b37be09c7c5f5ee1e1ccd5a0a40a
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2026-07-19 11:57:25 -0400

    Time: 394 ms (5.03%) | Memory: 23.3 MB (16.71%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        row = len(mat)
+        col = len(mat[0])
+
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+
+        ans = [[float('inf') for _ in range(col)] for _ in range(row)]
+        q = deque([])
+
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j] == 0:
+                    q.append((i, j, 0))
+
+        while q:
+            i, j, count = q.popleft()
+            mat[i][j] = -1
+            for m,n in directions:
+                ni = i+m
+                nj = j + n
+    
+                if 0<=ni<row and 0<=nj<col:
+                    if mat[ni][nj] == 1:
+                        ans[ni][nj] = min(ans[ni][nj], count + 1)
+                    if mat[ni][nj] != -1:
+                        q.append((ni, nj, count + 1))
+        
+        for i in range(row):
+            for j in range(col):
+                if ans[i][j] == float('inf'):
+                    ans[i][j] = 0
+        return ans
+
