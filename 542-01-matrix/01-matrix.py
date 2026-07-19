db419c119201b1faedd55c03da75f419882bcfe4
sanath kavatooru <67592639+sanath265@users.noreply.github.com>
2026-07-19 12:09:34 -0400

    Time: 145 ms (32.32%) | Memory: 22.4 MB (38.38%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -14,21 +14,17 @@ class Solution:
             for j in range(col):
                 if mat[i][j] == 0:
                     ans[i][j] = 0
-                    q.append((i, j, 0))
+                    q.append((i, j))
 
         while q:
-            i, j, count = q.popleft()
+            i, j = q.popleft()
             for m,n in directions:
                 ni = i+m
                 nj = j + n
     
                 if 0<=ni<row and 0<=nj<col and ans[ni][nj] == float('inf'):
                         ans[ni][nj] = ans[i][j] + 1
-                        q.append((ni, nj, ans[ni][nj]))
+                        q.append((ni, nj))
         
-        # for i in range(row):
-        #     for j in range(col):
-        #         if ans[i][j] == float('inf'):
-        #             ans[i][j] = 0
         return ans
 
